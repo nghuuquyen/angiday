da0bf114d6362a8f8ec19fb99ae665b3206c9b7f
Nguyen Quang An <ngquangan@gmail.com>
2018-10-26 22:49:59 +0700

    get data from mongodb success

--- api-server/api/services/GetDataDaily.js
+++ api-server/api/services/GetDataDaily.js
@@ -1,21 +1,77 @@
 /**
- * @name createMongodbIndexing
+ * @name getDataDaily
  * @module services
  * @description
- * Get food, keyword, shop, FoodKeywordRelation, ShopKeywordRelation from mongoDB to Neo4j
+ * Get food, keyword, shop, FoodKeywordRelation, FoodShopRelation, ShopKeywordRelation from mongoDB to Neo4j
  */
+
+const schedule = require('node-schedule');
 
 module.exports = {
     getData: getDataDaily
 };
 
 function getDataDaily() {
-  const db = Keyword.getDatastore().manager;
-  const keywordCollection = db.collection(Keyword.tableName);
-  const foodCollection = db.collection(Food.tableName);
-  const shopCollection = db.collection(Shop.tableName);
-  const foodKeywordRelationCollection = db.collection(FoodKeywordRelation.tableName);
-  const shopKeywordRelationCollection = db.collection(ShopKeywordRelation.tableName);
+  let getData = schedule.scheduleJob('*/1 * * * *', async function(){
+    let currentTime = new Date().getTime();
+    let foods = await Food.find(
+      {
+        updatedAt: {
+          '>=': currentTime - 86400000,
+          '<' : currentTime - 1000,
+        }
+      }
+    );
+
+    let keywords = await Keyword.find(
+      {
+        updatedAt: {
+          '>=': currentTime - 86400000,
+          '<' : currentTime - 1000,
+        }
+      }
+    );
+
+    let shops = await Shop.find(
+      {
+        updatedAt: {
+          '>=': currentTime - 86400000,
+          '<' : currentTime - 1000,
+        }
+      }
+    );
+
+    let foodKeywordRelations  = await FoodKeywordRelation.find(
+      {
+        updatedAt: {
+          '>=': currentTime - 86400000,
+          '<' : currentTime - 1000,
+        }
+      }
+    );
+
+    let foodShopRelations  = await FoodShopRelation.find(
+      {
+        updatedAt: {
+          '>=': currentTime - 86400000,
+          '<' : currentTime - 1000,
+        }
+      }
+    );
+
+    let shopKeywordRelations  = await ShopKeywordRelation.find(
+      {
+        updatedAt: {
+          '>=': currentTime - 86400000,
+          '<' : currentTime - 1000,
+        }
+      }
+    );
+
+
+
+  });
+
   sails.log("get Data from mongodb");
 
 }
